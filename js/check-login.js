a2b6b457508e6a30aacc71ee16d8df51b611d5c2
Ragim Guseynov <rmx.880@gmail.com>
2018-09-02 23:59:25 +0400

    login-form

--- js/check-login.js
+++ js/check-login.js
@@ -1,5 +1,49 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');	
+	(function(){
+
+		var formValidation = {
+
+			isValid: true,
+
+			init: function(){
+				//Вызов внутренних функций
+				this._setUpListeners();
+			},
+
+			_setUpListeners: function(){
+				$('#form-login').on('submit', formValidation._validateForm);
+			},
+
+			_validateForm: function(event){
+				event.preventDefault();
+				console.log(' privete _validateForm');
+				var formLogin = $(this),
+					input = formLogin.find('input'),
+					valid = true;
+
+				$.each( input, function(index, val){
+					var input = $(val),
+						value = input.val().trim(),
+						formGroup = input.find('#form-login');
+
+						if ( value.length === 0) {
+							formGroup.addClass('notify');
+						}
+						console.log('fdfd');
+
+				
+
+					
+				}); 
+			}
+
+
+		};
+
+		formValidation.init();
+
+	}());
+
 
 });
